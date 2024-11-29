abb0e8ffc72062c145b3b20ed4f2cdd17ba19e56
walter-eidi <walter-eidi@hotmail.com>
2024-11-29 20:16:31 -0300

    Adicionar arquivos de projeto.

---
+++ Uglyfy/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Uglyfy.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Uglyfy/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Uglyfy.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
